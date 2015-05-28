1a7e6e77e0f5ce9660f2d43c87b08159ecf4b946
Your Name <detzold@gmail.com>
2015-05-28 15:33:59 +0200

    moved

--- icmp_chat/rust/build.rs
+++ icmp_chat/rust/build.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    Command::new("gcc").args(&["src/net.c", "-c", "-fPIC", "-o"])
+    Command::new("gcc").args(&["c/net.c", "-c", "-fPIC", "-o"])
                        .arg(&format!("{}/net.o", out_dir)).status().unwrap();
     Command::new("ar").args(&["crus", "libicmp.a", "net.o"])
                       .current_dir(&Path::new(&out_dir)).status().unwrap();
